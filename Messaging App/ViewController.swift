5d7baca87e36b6032bcf846a73963fd3270f0185
SingwaneVD <94643260+SingwaneVD@users.noreply.github.com>
2022-08-24 10:55:13 +0200

    Initial Commit

---
+++ Messaging App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messaging App
+//
+//  Created by IACD-024 on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+}
+
